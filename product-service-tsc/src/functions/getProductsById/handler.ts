f7b58e756555331f5123b5f01e0695ea3029ca06
Elena Donina <Elena_Donina@epam.com>
2024-04-03 14:20:04 +0200

    Task-4

--- product-service-tsc/src/functions/getProductsById/handler.ts
+++ product-service-tsc/src/functions/getProductsById/handler.ts
@@ -1,15 +1,18 @@
 import { errorResponse, successResponse } from '../../utils/apiResponseBuilder';
-import { ProductServiceInterface } from '../../services/products';
+import { ProductServiceInterface, StockInterface } from '../../services/products';
+import { stockService } from "@functions/handlers";
 
 export const getProductsByIdHandler = (productService: ProductServiceInterface) => async (event, _context) => {
+  console.log('event', event);
   try {
     const { productId = '' } = event.pathParameters;
     const product = await productService.getProductById(productId);
+    const stocks: StockInterface[] = await stockService.getStockByProductId(productId);
 
     if (!product) {
       return errorResponse({name: '404', message: 'Product not found'}, 404);
     }
-    return successResponse(product);
+    return successResponse({ ...product, count: stocks?.length > 0 ? stocks[0].count : 0 });
   } catch (error) {
     return errorResponse({name: '500', message: 'Internal Error'}, 500);
   }
