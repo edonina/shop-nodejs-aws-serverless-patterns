f662b306b20647ab2843d4b58d75430c50a08d89
Elena Donina <Elena_Donina@epam.com>
2024-04-10 12:35:05 +0200

    task-5

---
+++ import-service/lib/src/functions/importProductsFile/handler.js
@@ -0,0 +1,19 @@
+import { formatJSONResponse } from '@libs/api-gateway';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { middyfy } from '@libs/lambda';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+const importProductsFile = async (event) => {
+    const client = new S3Client({ region: 'eu-west-1' });
+    const { name } = event.queryStringParameters;
+    const command = new PutObjectCommand({
+        Bucket: 'patterns-images',
+        Key: `uploaded/${name}`,
+    });
+    const signedUrl = await getSignedUrl(client, command, { expiresIn: 100000 });
+    return formatJSONResponse({
+        body: signedUrl,
+        event,
+    });
+};
+export const main = middyfy(importProductsFile);
+//# sourceMappingURL=handler.js.map
---
+++ import-service/lib/src/functions/index.js
@@ -0,0 +1,3 @@
+export { default as importFileParser } from './importFileParcer';
+export { default as importProductsFile } from './importProductsFile';
+//# sourceMappingURL=index.js.map
